4d12e8b51823fbe8ed3e17942202c57dfbf1058c
ali bijan <alibijan.98@gmail.com>
2019-02-22 02:09:43 -0500

    pushing files to work on them later

--- js/app.js
+++ js/app.js
@@ -1,5 +1,13 @@
-let navContainer = document.querySelector('.nav-container');
+const navContainer = document.querySelector('.nav-container')
 
-navContainer.addEventListener('click', function(){
-    
-})
+navContainer.addEventListener('click', navFunction);
+
+function navFunction() {
+    toggleClasses();
+}
+
+function toggleClasses() {
+    let navOverlay = navContainer.childNodes[3]
+    navOverlay.classList.toggle('expand');
+    console.log(navContainer.childNodes[3]);
+}
